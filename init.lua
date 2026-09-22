491825f985d8f2c7275eb31de7972adc7689aae4
João Tavares <joaotavares@MacBook-Air-de-Joao.local>
2026-09-22 12:09:55 -0300

    adding tokyonight theme

--- init.lua
+++ init.lua
@@ -283,6 +283,16 @@ require('lazy').setup({
     },
   },
   {
+    'tiagovla/tokyodark.nvim',
+    opts = {
+      -- custom options here
+    },
+    config = function(_, opts)
+      require('tokyodark').setup(opts) -- calling setup is optional
+      vim.cmd [[colorscheme tokyodark]]
+    end,
+  },
+  {
     'romgrk/barbar.nvim',
     dependencies = {
       'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
